c3c1d5f6ecfd76f6427d2197b10e97d67bba9aac
Sagakichi <sebastian.jouannet2101@alumnos.ubiobio.cl>
2025-01-31 15:21:41 -0300

    Cambios para usarse con web service de azure

--- src/Environments/environment.ts
+++ src/Environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrlCookies: 'https://localhost:7017/api/cookies',
-    apiUrlPolitics: 'https://localhost:7017/api/politics'
+    apiUrlCookies: 'https://backendki-g6g5b3bdardueqdp.brazilsouth-01.azurewebsites.net/api/cookies',
+    apiUrlPolitics: 'https://backendki-g6g5b3bdardueqdp.brazilsouth-01.azurewebsites.net/api/politics'
   }
   
