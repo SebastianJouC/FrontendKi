86d62742b5fbffdd6932514939b7e842927cc19a
Sagakichi <sebastian.jouannet2101@alumnos.ubiobio.cl>
2025-02-03 11:57:07 -0300

    Cambios minimos

--- src/Environments/environment.ts
+++ src/Environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,
+    production: false,
     apiUrlCookies: 'https://backendki-g6g5b3bdardueqdp.brazilsouth-01.azurewebsites.net/api/cookies',
     apiUrlPolitics: 'https://backendki-g6g5b3bdardueqdp.brazilsouth-01.azurewebsites.net/api/politics'
   }
